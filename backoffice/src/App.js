f80955cc22b27a802a05c0d7c43012d167ec81b4
Bruno Paixao <brunomrpx@gmail.com>
2018-07-07 01:49:40 -0300

    Added bulma CSS framework for Backoffice

--- backoffice/src/App.js
+++ backoffice/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 
+import 'bulma/css/bulma.css'
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,8 @@ class App extends Component {
     this.state = {
       queue: [],
       finished: [],
-      selected: []
+      selected: [],
+      activeTab: 'selected'
     };
 
     this.socket = io.connect('http://localhost:3000');
@@ -36,61 +38,100 @@ class App extends Component {
     this.socket.emit('finish-token', token.id);
   }
 
-  getRows(queue) {
-    if (queue && queue.length > 0) {
-      return queue.map(token => (
-        <tr key={token.id}>
-          <td>{token.id}</td>
-          <td>{token.createDate}</td>
+  getRows(queue, showFinish) {
+    if (!queue || queue.length === 0) {
+      return (
+        <tr>
+          <td>No token found</td>
         </tr>
-      ));
+      );
     }
     
-    return (
-      <tr>
-        <td>There is no token created</td>
+    return queue.map(token => (
+      <tr key={token.id}>
+        <td>{token.id}</td>
+        <td>{token.createDate}</td>
+        { showFinish && (
+          <td>
+            <button className="button is-warning" onClick={() => this.finishToken(token)}>Finish</button>
+          </td>
+        )}
       </tr>
+    ));
+  }
+
+  getTable(tokens, showFinish = false) {
+    return (
+      <table className="table" style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Date</th>
+            { showFinish && <th></th>}
+          </tr>
+        </thead>
+        <tbody>
+          {this.getRows(tokens, showFinish)}
+        </tbody>
+      </table>
     );
   }
 
   showSelectedTokens(tokens) {
     if (tokens.length === 0) {
-      return <div>No selected tokens</div>;
+      return <div className="column">No selected tokens</div>;
     }
     
     return (
-      <ul>
-        {tokens.map(token => (
-          <li key={token.id}>{token.createDate} - {token.id} <button onClick={() => this.finishToken(token)}>Finish</button></li>
-        ))}
-      </ul>
+      <div>
+        <ul>
+          {tokens.map(token => (
+            <li key={token.id}>
+              {token.createDate} - {token.id} 
+              <button className="button is-warning" onClick={() => this.finishToken(token)}>Finish</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
+  }
+
+  selectTab(name) {
+    this.setState({ activeTab: name });
   }
 
   render() {
     const { queue, selected } = this.state;
+    const tabContent = { queue, selected };
+    const showFinish = this.state.activeTab === 'selected';
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Token Care Backoffice</h1>
-        </header>
-        <p className="App-intro">
-          <button onClick={() => this.createToken()}>Create Token</button>
-          <button onClick={() => this.next()}>Next</button>
-        </p>
-        {this.showSelectedTokens(selected)}
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.getRows(queue)}
-          </tbody>
-        </table>
+      <div>
+        <div className="container" style={{ paddingTop: '10px' }}>
+          <div className="columns">
+            <div className="column">
+              <div className="field is-grouped">
+                <p className="control">
+                  <button className="button is-primary" onClick={() => this.createToken()}>Create Token</button>
+                </p>
+                <p className="control">
+                  <button className="button is-info" onClick={() => this.next()}>Next</button>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="tabs">
+            <ul>
+              <li className={this.state.activeTab === 'selected' ? 'is-active' : ''} onClick={() => this.selectTab('selected')}>
+                <a>Selected</a>
+              </li>
+              <li className={this.state.activeTab === 'queue' ? 'is-active' : ''} onClick={() => this.selectTab('queue')}>
+                <a>Queue</a>
+              </li>
+            </ul>
+          </div>
+          {this.getTable(tabContent[this.state.activeTab], showFinish)}
+        </div>
       </div>
     );
   }
